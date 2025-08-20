f34fa10cd35015b40707c028dac0467e471c4650
Jennifer MERAT <131549759+refinejcode49@users.noreply.github.com>
2025-08-20 20:53:30 +0200

    added authentification code

--- brut/src/components/App/index.tsx
+++ brut/src/components/App/index.tsx
@@ -1,10 +1,26 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Login from "../Login"
 import TrackInfo from "../TrackInfo"
+import { getAccessToken } from "../../../auth"
 
 const App = () => {
 
   const [token, setToken] = useState<string | null>(null)
+
+  useEffect(() => {
+    getToken()
+  })
+
+  const getToken = async () => {
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if(code){
+      const accessToken = await getAccessToken(clientId, code)
+      setToken(accessToken)
+  }
+  }
 
 
   if (!token) {
--- brut/src/components/Nav/index.tsx
+++ brut/src/components/Nav/index.tsx
@@ -1,11 +1,21 @@
-
+import { redirectToAuthCodeFlow } from "../../../auth"
 
 const Nav = () => {
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+
+
+    const handleClick = async () => {
+        await redirectToAuthCodeFlow(clientId)
+    }
+
+
 
     return (
         <>
-        <h1>Brut</h1>
-        <button>Login</button>
+            <h1>Brut</h1>
+            <button onClick={handleClick}>
+                Login
+            </button>
         </>
     )
 }
